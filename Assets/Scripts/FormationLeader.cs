29f002b4346c84113f2d687638f7667ad30fb9a9
swood456 <crazedsportsfan@gmail.com>
2017-10-14 12:43:42 -0400

    started work on making leader move around level

---
+++ Assets/Scripts/FormationLeader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormationLeader : MonoBehaviour {
+
+    public Vector2 dest;
+    public float max_speed;
+    public float acceleration;
+    public float rotational_speed;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        // rotate
+        Vector2 _direction = (dest - (Vector2)transform.position).normalized;
+        float angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+        
+        float lerp_angle = Mathf.LerpAngle(transform.eulerAngles.z, angle, Time.deltaTime * rotational_speed);
+        transform.rotation = Quaternion.Euler(0f, 0f, lerp_angle);
+        
+        // accelerate
+        rb.AddForce(transform.right);
+        if(rb.velocity.magnitude > max_speed)
+        {
+            rb.velocity = rb.velocity.normalized * max_speed;
+        }
+	}
+}
