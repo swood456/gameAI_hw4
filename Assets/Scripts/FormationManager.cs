e8bbbaf8bf553997646530a049fe5a637bb87446
Stephanie Manley <manles@rpi.edu>
2017-10-17 19:17:35 -0400

    Formation updates
    circle formation added

--- Assets/Scripts/FormationManager.cs
+++ Assets/Scripts/FormationManager.cs
@@ -2,48 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct Slot
+{
+    public Vector2 pos;
+    public FormationMember member;
+
+    public void Set(Vector2 l)
+    {
+        member.dest = l + pos;
+    }
+}
+
 public class FormationManager : MonoBehaviour {
 
     public float group_scalar = 2.0f;
     public FormationLeader leader;
     public List<FormationMember> members;
+    public List<Slot> slots;
 
 
 	// Use this for initialization
 	void Start () {
+        leader = GetComponentInChildren<FormationLeader>();
         foreach (FormationMember child in GetComponentsInChildren<FormationMember>())
         {
             members.Add(child);
         }
+        slots = new List<Slot>();
         setup_members();
     }
 
     private void Update()
     {
-        setup_members();
+        Vector2 lead = leader.transform.position;
+        foreach (Slot mem in slots)
+        {
+            mem.Set(lead);
+        }
     }
 
     void setup_members()
     {
-        Vector2[] pos = setup_group(members.Count);
-
-        for (int i = 0; i < members.Count; ++i)
-        {
-            members[i].dest = pos[i];
-        }
+        setup_circle(members.Count);
     }
 
-    Vector2[] setup_group(int num_members)
+    void setup_circle(int num_members)
     {
-        Vector2[] member_pos = new Vector2[num_members];
+        List<Vector2> member_pos = new List<Vector2>();
 
         // for now assume circle shape
         float group_radius = Mathf.Sqrt(group_scalar * num_members / Mathf.PI);
 
         Vector2 mid = leader.transform.position - leader.transform.right * group_radius;
+        print("right =" + leader.transform.right + " mid = " + mid);
 
         // update the leader's velocity based on members dist from center
-        float dist_total = 0.0f;
+        //float dist_total = 0.0f;
 
         for (int i = 1; i <= num_members; ++i)
         {
@@ -51,53 +65,55 @@ public class FormationManager : MonoBehaviour {
             Vector2 agent_pos;
             agent_pos.x = Mathf.Cos(2 * Mathf.PI * ((float)i / (num_members + 1))) * group_radius + mid.x;
             agent_pos.y = Mathf.Sin(2 * Mathf.PI * ((float)i / (num_members + 1))) * group_radius + mid.y;
-            member_pos[i-1] = agent_pos;
+
+            member_pos.Add(agent_pos);
 
             //dist_total += members[i-1].transform;
-            dist_total += ((Vector2)members[i-1].transform.position - mid).magnitude;
+            //dist_total += ((Vector2)members[i-1].transform.position - mid).magnitude;
         }
 
-        float avg_dist = dist_total / num_members;
-        if(avg_dist > group_radius)
-        {
-            leader.slowdown_leader(group_radius / avg_dist);
-        }
-        else
-        {
-            leader.slowdown_leader(avg_dist / group_radius);
-        }
-        
+        //float avg_dist = dist_total / num_members;
+        //if(avg_dist > group_radius)
+        //{
+        //    leader.slowdown_leader(group_radius / avg_dist);
+        //}
+        //else
+        //{
+        //    leader.slowdown_leader(avg_dist / group_radius);
+        //}
 
-        return member_pos;
+        assignMembers(member_pos);
     }
 
-    /* this is mostly unfinished
-    public void assignMembers(Vector2[] pos)
+    public void assignMembers(List<Vector2> s)
     {
-        List<FormationMember> rem = members;
-        foreach (Vector2 p in pos)
+        foreach (FormationMember m in members)
         {
-            FormationMember bestFit;
+            Vector2 bestFit = Vector2.zero;
             float bestDist = Mathf.Infinity;
-            foreach(FormationMember m in rem)
+            foreach(Vector2 t in s)
             {
-                float dist = Vector2.Distance(p, m.transform.position);
+                float dist = Vector2.Distance(t, m.transform.position);
                 if (dist < bestDist)
                 {
-                    bestFit = m;
+                    bestFit = t;
                     bestDist = dist;
-                }
-
-                bestFit.dest = 
+                }               
             }
+            Slot tmp;
+            tmp.pos = bestFit;
+            tmp.member = m;
+
+            slots.Add(tmp);
+
+            s.Remove(bestFit);
         }
     }
-    */
 
     public void RemoveAgent(FormationMember f)
     {
         members.Remove(f);
         setup_members();
-    }   
+    }
 
 }
--- Assets/Scripts/FormationMember.cs
+++ Assets/Scripts/FormationMember.cs
@@ -4,63 +4,73 @@ using UnityEngine;
 
 public class FormationMember : MonoBehaviour {
 
+
     public Vector2 dest;
 
     Rigidbody2D rb;
     public float acceleration;
     public float max_speed;
     public float rotational_speed;
+
 	
 	void Start () {
-        //dest = transform.position;
         rb = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        //transform.position = dest;
 
-        // seek towards dest
-        /*
-        rb.AddForce((dest - (Vector2)transform.position).normalized * acceleration);
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.AddForce(dest - rb.position);
         if (rb.velocity.magnitude > max_speed)
-            rb.velocity = (dest - (Vector2)transform.position).normalized * max_speed;
-        */
-
-        // rotate if we are far enough away
-        if(((Vector2)transform.position - dest).magnitude > 0.25f)
         {
-            Vector2 _direction = (dest - (Vector2)transform.position).normalized;
-            float angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
-
-            float lerp_angle = Mathf.LerpAngle(transform.eulerAngles.z, angle, Time.deltaTime * rotational_speed);
-            transform.rotation = Quaternion.Euler(0f, 0f, lerp_angle);
-        }
-
-        if(Vector2.Dot((Vector2)transform.position - dest, transform.right) < 0)
-        {
-            // accelerate
-            rb.AddForce(transform.right * acceleration);
-            if (rb.velocity.magnitude > max_speed)
-            {
-                rb.velocity = rb.velocity.normalized * max_speed;
-            }
+            rb.velocity.Normalize();
+            rb.velocity *= max_speed;
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        print("collision start!");
-        if(collision.gameObject.GetComponent<BlackBird>())
-        {
-            FormationManager f_manager = FindObjectOfType<FormationManager>();
-            f_manager.RemoveAgent(this);
-            Destroy(this.gameObject);
-        }
-    }
+    //    //transform.position = dest;
 
-    private void emergDest()
-    {
+    //    // seek towards dest
+    //    /*
+    //    rb.AddForce((dest - (Vector2)transform.position).normalized * acceleration);
+    //    if (rb.velocity.magnitude > max_speed)
+    //        rb.velocity = (dest - (Vector2)transform.position).normalized * max_speed;
+    //    */
 
-    }
+    //    // rotate if we are far enough away
+    //    if(((Vector2)transform.position - dest).magnitude > 0.25f)
+    //    {
+    //        Vector2 _direction = (dest - (Vector2)transform.position).normalized;
+    //        float angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+
+    //        float lerp_angle = Mathf.LerpAngle(transform.eulerAngles.z, angle, Time.deltaTime * rotational_speed);
+    //        transform.rotation = Quaternion.Euler(0f, 0f, lerp_angle);
+    //    }
+
+    //    if(Vector2.Dot((Vector2)transform.position - dest, transform.right) < 0)
+    //    {
+    //        // accelerate
+    //        rb.AddForce(transform.right * acceleration);
+    //        if (rb.velocity.magnitude > max_speed)
+    //        {
+    //            rb.velocity = rb.velocity.normalized * max_speed;
+    //        }
+    //    }
+    //}
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    print("collision start!");
+    //    if(collision.gameObject.GetComponent<BlackBird>())
+    //    {
+    //        FormationManager f_manager = FindObjectOfType<FormationManager>();
+    //        f_manager.RemoveAgent(this);
+    //        Destroy(this.gameObject);
+    //    }
+    //}
 }
