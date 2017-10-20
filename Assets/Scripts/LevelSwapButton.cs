4b822af27cc594e5ce4864581f00ae41db1ad990
swood456 <crazedsportsfan@gmail.com>
2017-10-20 15:50:10 -0400

    added second secene for emergent and added button for it

---
+++ Assets/Scripts/LevelSwapButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelSwapButton : MonoBehaviour {
+
+    public void swap_level(string level_name)
+    {
+        SceneManager.LoadScene(level_name);
+    }
+}
